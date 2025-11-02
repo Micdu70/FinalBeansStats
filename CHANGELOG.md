d5437c3cdfdf067fc534c55aa222a402f95b3bdd
Micdu70 <Micdu70@gmx.fr>
2025-11-02 23:08:24 +0100

    v1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v1.1
+- General bug fixes & improvements
+
 ## v1.0
 - Initial Relase (for FinalBeans Season 2!)
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0f163249-ed8d-4ef5-ab07-0cca03948f44")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
